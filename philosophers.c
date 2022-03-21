eaaeb48dbed5d5a191e34b9c91d294d94bc214fd
Alef <alefff.gabriel@gmail.com>
2022-03-21 16:53:36 -0300

    cheacker ok

---
+++ philosophers.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+static int	checker_digit(char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+			{
+				printf("error : invalid number in arguments\n");
+				return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+static int	checker(int argc, char **argv)
+{
+	if (argc > 5)
+	{
+		printf("error : too many arguments\n");
+		return (1);
+	}
+	else if (argc < 5)
+	{
+		printf("error : invalid arguments\n");
+		return (1);
+	}
+	else if (checker_digit(argv) == 1)
+		return (1);
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (checker(argc, argv) == 0)
+		return (2);
+	return (0);
+}
