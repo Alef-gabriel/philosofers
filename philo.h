0b35bf5604e3c4df6edb7a0d338ef0798b8ec2d3
Alef <alefff.gabriel@gmail.com>
2022-03-23 01:33:05 -0300

    thread consept

--- philo.h
+++ philo.h
@@ -3,13 +3,20 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_philothread{
+	pthread_t		*th;
+	int				id;
+}	t_pthred;
 
 typedef struct s_philosophers{
-	int	number_of_philosophers;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	number_of_times_each_philosopher_must_eat;
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				number_of_times_each_philosopher_must_eat;
+	t_pthred		**info;
 }	t_philo;
 
 int	checker_digit(char **argv);
--- philosophers.c
+++ philosophers.c
@@ -1,11 +1,62 @@
 #include "philo.h"
 
+pthread_mutex_t	mut;
+
+void	*hander(void *args)
+{
+	t_pthred	*info;
+
+	info = (t_pthred *)args;
+	pthread_mutex_lock(&mut);
+	printf("hey thread %d\n", info->id);
+	usleep(100000);
+	pthread_mutex_unlock(&mut);
+}
+
+t_pthred	**info_inicialize(int size)
+{
+	pthread_t	*th;
+	t_pthred	**info;
+	int			i;
+
+	i = 0;
+	info = (t_pthred **)malloc(sizeof(t_pthred *) * size);
+	while (i < size)
+	{
+		info[i] = (t_pthred *)malloc(sizeof(t_pthred));
+		info[i]->id = i;
+		info[i]->th = (pthread_t *)malloc(sizeof(pthread_t));
+		i++;
+	}
+	return (info);
+}
+
 int	main(int argc, char *argv[])
 {
-	t_philo	philo;
+	t_philo		philo;
+	int i = 0;
 
 	if (checker(argc, argv) == 1)
 		return (1);
 	parce(argv, &philo);
+	philo.info = info_inicialize(philo.number_of_philosophers);
+	pthread_mutex_init(&mut, NULL);
+	while (philo.number_of_times_each_philosopher_must_eat)
+	{
+		i = 0;
+		while (i < philo.number_of_philosophers)
+		{
+			pthread_create(philo.info[i]->th, NULL, &hander, philo.info[i]);
+			i++;
+		}
+		philo.number_of_times_each_philosopher_must_eat--;
+	}
+	i = 0;
+	while (i < philo.number_of_philosophers)
+	{
+		pthread_join(*(philo.info[i]->th), NULL);
+		i++;
+	}
+	pthread_mutex_destroy(&mut);
 	return (0);
 }
